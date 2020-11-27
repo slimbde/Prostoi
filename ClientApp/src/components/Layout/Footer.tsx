9c228027bb10176cfda52bf5f2d5c086fa27d67f
slimbde@gmail.com <grigoriydolgiy@mechel.ru>
2020-11-27 15:05:48 +0500

    fixed idle query, lost cast query

--- ClientApp/src/components/Layout/Footer.tsx
+++ ClientApp/src/components/Layout/Footer.tsx
@@ -1,4 +1,4 @@
 import * as React from 'react'
 export default () => <div className="footer">
-  <a href="mailto:GrigoriyDolgiy@mechel.ru">Версия: 13.11.2020.01</a>
+  <a href="mailto:GrigoriyDolgiy@mechel.ru">Версия: 27.11.2020.01</a>
 </div >
--- Models/Repositories/L2Reposotory.cs
+++ Models/Repositories/L2Reposotory.cs
@@ -33,8 +33,8 @@ namespace rest_ts_react_template.Models.Repositories
       var stmt = @"
         WITH p AS (
           SELECT 
-            TO_DATE(:bDate,'YYYY-MM-DD')                      startT, 
-            TO_DATE(:eDate,'YYYY-MM-DD') + interval '1' day    stopT 
+          TO_DATE(:bDate,'YYYY-MM-DD')                      startT, 
+          TO_DATE(:eDate,'YYYY-MM-DD') + interval '1' day    stopT 
           FROM dual
         )
         SELECT 
@@ -43,35 +43,36 @@ namespace rest_ts_react_template.Models.Repositories
           outtr.practice_id as pId, 										
           L2PRD.CCM_GENERAL_PROGRAM_VARS.NUMERIC_VALUE as density,	
           L2PRD.REP_CCM_PRODUCT_ORDERS.WIDTH as width,						
-          L2PRD.REP_CCM_PRODUCT_ORDERS.THICKNESS as thickness,					
+          L2PRD.REP_CCM_PRODUCT_ORDERS.THICKNESS as thickness,		
+          --DELAYS.timeinterval as realinterval,
           CASE WHEN DELAYS.timeinterval < 0 THEN 0 ELSE ROUND(DELAYS.timeinterval * 5, 0) END as count
         FROM ( 
           SELECT 
-            STOP_DATE,
-            MAX(innr.cnt) KEEP (DENSE_RANK LAST ORDER BY cnt) cnt,
-            MAX(innr.REPORT_COUNTER) KEEP (DENSE_RANK LAST ORDER BY cnt) REPORT_COUNTER,
-            MAX(innr.PRACTICE_ID) KEEP (DENSE_RANK LAST ORDER BY cnt) PRACTICE_ID,
-            MAX(innr.STEEL_GRADE_ID) KEEP (DENSE_RANK LAST ORDER BY cnt) STEEL_GRADE_ID
+          STOP_DATE,
+          MAX(innr.cnt) KEEP (DENSE_RANK LAST ORDER BY cnt) cnt,
+          MAX(innr.REPORT_COUNTER) KEEP (DENSE_RANK LAST ORDER BY cnt) REPORT_COUNTER,
+          MAX(innr.PRACTICE_ID) KEEP (DENSE_RANK LAST ORDER BY cnt) PRACTICE_ID,
+          MAX(innr.STEEL_GRADE_ID) KEEP (DENSE_RANK LAST ORDER BY cnt) STEEL_GRADE_ID
           FROM ( 
+          SELECT 
+            STOP_DATE,
+            STEEL_GRADE_ID,
+            PRACTICE_ID,
+            MIN(REPORT_COUNTER) REPORT_COUNTER,
+            COUNT(*) cnt
+          FROM 
+            (
             SELECT 
-              STOP_DATE,
-              STEEL_GRADE_ID,
-              PRACTICE_ID,
-              MIN(REPORT_COUNTER) REPORT_COUNTER,
-              COUNT(*) cnt
-            FROM 
-                (
-              SELECT 
-                steel_grade_id,
-                practice_id,
-                TRUNC(STOP_DATE + INTERVAL '4' hour + INTERVAL '30' minute, 'DDD') as STOP_DATE,
-                report_counter
-              FROM L2PRD.REPORTS, p
-              WHERE 
-                STOP_DATE + INTERVAL '4' hour + INTERVAL '30' minute >= p.startT
-                and STOP_DATE + INTERVAL '4' hour + INTERVAL '30' minute < p.stopT
-            )
-            GROUP BY STOP_DATE, steel_grade_id, practice_id
+            steel_grade_id,
+            practice_id,
+            TRUNC(STOP_DATE + INTERVAL '4' hour + INTERVAL '30' minute, 'DDD') as STOP_DATE,
+            report_counter
+            FROM L2PRD.REPORTS, p
+            WHERE 
+            STOP_DATE + INTERVAL '4' hour + INTERVAL '30' minute >= p.startT
+            and STOP_DATE + INTERVAL '4' hour + INTERVAL '30' minute < p.stopT
+          )
+          GROUP BY STOP_DATE, steel_grade_id, practice_id
           ) innr 
           GROUP BY STOP_DATE
         ) outtr
@@ -86,57 +87,57 @@ namespace rest_ts_react_template.Models.Repositories
 
         JOIN (
           SELECT 
-            delayDate,
-            sum(timeinterval) as timeinterval
+          delayDate,
+          sum(timeinterval) as timeinterval
           FROM (
-            SELECT 
-              TRUNC(startS, 'DDD') as delayDate,
-              startS as startShifted,
-              stopS as stopShifted,
-              startS - INTERVAL '4' hour - INTERVAL '30' minute as startTime,
-              stopS - INTERVAL '4' hour - INTERVAL '30' minute as stopTime,
-              (stopS - startS) * 24 * 60 * 60 as timeinterval
-            FROM (
-              WITH rws as (
-                select 
-                  rownum r 
-                from dual 
-                connect by level <= 2
-              )
-              SELECT 
-                CASE r
-                  WHEN 1 THEN TRUNC(delay_stop_shift, 'DDD')
-                  ELSE delay_start_shift
-                END as startS,
-                
-                CASE r
-                  WHEN 2 THEN 
-                    CASE 
-                      WHEN EXTRACT(DAY from cast(delay_start_shift as timestamp)) <> EXTRACT(DAY from cast(delay_stop_shift as timestamp)) THEN TRUNC(delay_stop_shift, 'DDD')
-                      ELSE delay_stop_shift
-                    END
-                  ELSE delay_stop_shift
-                END as stopS
-              FROM 
-                    (
-                SELECT 
-                  delay_start_time, 																	
-                  COALESCE(NULLIF(delay_stop_time, TO_DATE('01-01-1970', 'DD-MM-YYYY')), SYSDATE) as delay_stop_time,																	
-                  delay_start_time + INTERVAL '4' hour + INTERVAL '30' minute as delay_start_shift,	
-                  COALESCE(NULLIF(delay_stop_time, TO_DATE('01-01-1970', 'DD-MM-YYYY')), SYSDATE) + INTERVAL '4' hour + INTERVAL '30' minute as delay_stop_shift		
-                FROM L2PRD.REP_DELAYS, p
-                WHERE 
-                  delay_start_time + INTERVAL '4' hour + INTERVAL '30' minute >= p.startT	
-                  AND delay_start_time + INTERVAL '4' hour + INTERVAL '30' minute < p.stopT
-                    )
-                
-                
-              JOIN rws 
-                on CASE
-                  WHEN EXTRACT(DAY from cast(delay_start_shift as timestamp)) = EXTRACT(DAY from cast(delay_stop_shift as timestamp)) THEN 2 		
-                  ELSE 1 																															
-                END <= rws.r
+          SELECT 
+            TRUNC(startS, 'DDD') as delayDate,
+            startS as startShifted,
+            stopS as stopShifted,
+            startS - INTERVAL '4' hour - INTERVAL '30' minute as startTime,
+            stopS - INTERVAL '4' hour - INTERVAL '30' minute as stopTime,
+            (stopS - startS) * 24 * 60 * 60 as timeinterval
+          FROM (
+            WITH rws as (
+            select 
+              rownum r 
+            from dual 
+            connect by level <= 2
             )
+            SELECT 
+            CASE r
+              WHEN 1 THEN TRUNC(delay_stop_shift, 'DDD')
+              ELSE delay_start_shift
+            END as startS,
+            
+            CASE r
+              WHEN 2 THEN 
+              CASE 
+                WHEN EXTRACT(DAY from cast(delay_start_shift as timestamp)) <> EXTRACT(DAY from cast(delay_stop_shift as timestamp)) THEN TRUNC(delay_stop_shift, 'DDD')
+                ELSE delay_stop_shift
+              END
+              ELSE delay_stop_shift
+            END as stopS
+            FROM 
+              (
+            SELECT delay_start_time, 																	
+              COALESCE(NULLIF(delay_stop_time, TO_DATE('01-01-1970', 'DD-MM-YYYY')), SYSDATE) as delay_stop_time,																	
+              delay_start_time + INTERVAL '4' hour + INTERVAL '30' minute as delay_start_shift,	
+              COALESCE(NULLIF(delay_stop_time, TO_DATE('01-01-1970', 'DD-MM-YYYY')), SYSDATE) + INTERVAL '4' hour + INTERVAL '30' minute as delay_stop_shift		
+            FROM L2PRD.REP_DELAYS, p
+            WHERE 
+              (DELAY_START_TIME + INTERVAL '4' hour + INTERVAL '30' minute >= p.startT
+              AND DELAY_START_TIME + INTERVAL '4' hour + INTERVAL '30' minute < p.stopT)
+                  OR
+                  (DELAY_STOP_TIME + INTERVAL '4' hour + INTERVAL '30' minute >= p.startT
+              AND DELAY_STOP_TIME + INTERVAL '4' hour + INTERVAL '30' minute < p.stopT)
+              )
+            JOIN rws 
+            on CASE
+              WHEN EXTRACT(DAY from cast(delay_start_shift as timestamp)) = EXTRACT(DAY from cast(delay_stop_shift as timestamp)) THEN 2 		
+              ELSE 1 																															
+            END <= rws.r
+          )
           )
           GROUP BY delayDate 
         ) DELAYS 
@@ -181,89 +182,92 @@ namespace rest_ts_react_template.Models.Repositories
             from dual
           )
           SELECT 
-              outtr.STOP_DATE,
-              --outtr.steel_grade_id, 
-              --outtr.practice_id, 
-              CCM_GENERAL_PROGRAM_VARS.NUMERIC_VALUE DENSITY,
-              REP_CCM_PRODUCT_ORDERS.WIDTH,
-              REP_CCM_PRODUCT_ORDERS.THICKNESS,
-              case when DELAYS.Undercast_length < 0 then 0 else ROUND(DELAYS.Undercast_length, 15) end as UNDERCAST_LENGTH --(mm)
+            outtr.STOP_DATE,
+            --outtr.steel_grade_id, 
+            --outtr.practice_id, 
+            CCM_GENERAL_PROGRAM_VARS.NUMERIC_VALUE DENSITY,
+            REP_CCM_PRODUCT_ORDERS.WIDTH,
+            REP_CCM_PRODUCT_ORDERS.THICKNESS,
+            case when DELAYS.Undercast_length < 0 then 0 else ROUND(DELAYS.Undercast_length, 15) end as UNDERCAST_LENGTH --(mm)
           FROM (   
+            SELECT STOP_DATE,
+              MAX(innr.cnt) KEEP (DENSE_RANK LAST ORDER BY cnt) cnt,
+              MAX(innr.REPORT_COUNTER)  KEEP (DENSE_RANK LAST ORDER BY cnt) REPORT_COUNTER,
+              MAX(innr.PRACTICE_ID)  KEEP (DENSE_RANK LAST ORDER BY cnt) PRACTICE_ID,
+              MAX(innr.STEEL_GRADE_ID)  KEEP (DENSE_RANK LAST ORDER BY cnt) STEEL_GRADE_ID
+            FROM (   
               SELECT STOP_DATE,
-                    MAX(innr.cnt) KEEP (DENSE_RANK LAST ORDER BY cnt) cnt,
-                    MAX(innr.REPORT_COUNTER)  KEEP (DENSE_RANK LAST ORDER BY cnt) REPORT_COUNTER,
-                    MAX(innr.PRACTICE_ID)  KEEP (DENSE_RANK LAST ORDER BY cnt) PRACTICE_ID,
-                    MAX(innr.STEEL_GRADE_ID)  KEEP (DENSE_RANK LAST ORDER BY cnt) STEEL_GRADE_ID
-              FROM (   
-                  SELECT STOP_DATE,
-                        STEEL_GRADE_ID,
-                        PRACTICE_ID,
-                        MIN(REPORT_COUNTER) REPORT_COUNTER,
-                        COUNT(*) cnt
-                  FROM (
-                      SELECT steel_grade_id,
-                            practice_id,
-                            TRUNC(STOP_DATE + INTERVAL '4' hour + INTERVAL '30' minute, 'DDD') as STOP_DATE,
-                            report_counter
-                      FROM L2PRD.REPORTS, p
-                      WHERE 
-                          STOP_DATE + INTERVAL '4' hour + INTERVAL '30' minute >= p.startT
-                          and STOP_DATE + INTERVAL '4' hour + INTERVAL '30' minute < p.stopT
-                  )
-                  GROUP BY STOP_DATE, steel_grade_id, practice_id
-              ) innr 
-              GROUP BY STOP_DATE
+                STEEL_GRADE_ID,
+                PRACTICE_ID,
+                MIN(REPORT_COUNTER) REPORT_COUNTER,
+                COUNT(*) cnt
+              FROM (
+                SELECT steel_grade_id,
+                  practice_id,
+                  TRUNC(STOP_DATE + INTERVAL '4' hour + INTERVAL '30' minute, 'DDD') as STOP_DATE,
+                  report_counter
+                FROM L2PRD.REPORTS, p
+                WHERE 
+                  STOP_DATE + INTERVAL '4' hour + INTERVAL '30' minute >= p.startT
+                  and STOP_DATE + INTERVAL '4' hour + INTERVAL '30' minute < p.stopT
+              )
+              GROUP BY STOP_DATE, steel_grade_id, practice_id
+            ) innr 
+            GROUP BY STOP_DATE
           ) outtr
           JOIN L2PRD.CCM_GENERAL_PROGRAM_VARS
           ON L2PRD.CCM_GENERAL_PROGRAM_VARS.PRACTICE_ID = outtr.practice_id AND L2PRD.CCM_GENERAL_PROGRAM_VARS.VAR_CODE = 52003 AND AREA_ID = 1100
           JOIN L2PRD.REP_CCM_PRODUCT_ORDERS
           ON outtr.REPORT_COUNTER = REP_CCM_PRODUCT_ORDERS.REPORT_COUNTER
           JOIN (
-              SELECT delayDate,
-                    sum(Undercast_length) as Undercast_length --(mm)
+            SELECT delayDate,
+              sum(Undercast_length) as Undercast_length --(mm)
+            FROM (
+              SELECT TRUNC(startS, 'DDD') as delayDate,
+                startS as startShifted,
+                stopS as stopShifted,
+                startS - INTERVAL '4' hour - INTERVAL '30' minute as startTime,
+                stopS - INTERVAL '4' hour - INTERVAL '30' minute as stopTime ,
+                Undercast_length
               FROM (
-                  SELECT TRUNC(startS, 'DDD') as delayDate,
-                        startS as startShifted,
-                        stopS as stopShifted,
-                        startS - INTERVAL '4' hour - INTERVAL '30' minute as startTime,
-                        stopS - INTERVAL '4' hour - INTERVAL '30' minute as stopTime ,
-                        Undercast_length
-                  FROM (
-                      with rws as (
-                          select rownum r from dual connect by level <= 2
-                      )
-                      SELECT Undercast_length,
-                            case r
-                              when 1 then TRUNC(delay_stop_shift, 'DDD')
-                              else delay_start_shift
-                              end as startS,
-                            case r
-                              when 2 then 
-                              CASE 
-                                  WHEN EXTRACT(DAY from cast(delay_start_shift as timestamp)) <> EXTRACT(DAY from cast(delay_stop_shift as timestamp))  
-                                  THEN TRUNC(delay_stop_shift, 'DDD')
-                              else delay_stop_shift
-                              END
-                            else delay_stop_shift  
-                            end as stopS
-                      FROM (
-                          SELECT (length_at_end - length_at_beginning) * (  -0.01 *percentage_of_deviation / (1-0.01 * percentage_of_deviation)) as Undercast_length,
-                                start_downtime, 
-                                end_downtime,
-                                start_downtime + INTERVAL '4' hour + INTERVAL '30' minute as delay_start_shift,
-                                end_downtime + INTERVAL '4' hour + INTERVAL '30' minute as delay_stop_shift
-                          FROM L2PRD.REP_CCM_DOWNTIME, p
-                          WHERE start_downtime + INTERVAL '4' hour + INTERVAL '30' minute >= p.startT
-                          AND start_downtime + INTERVAL '4' hour + INTERVAL '30' minute < p.stopT
-                      )
-                      JOIN rws on case 
-                                    when EXTRACT(DAY from cast(delay_start_shift as timestamp)) = EXTRACT(DAY from cast(delay_stop_shift as timestamp)) 
-                                    then 2 
-                                    else 1 
-                                  end <= rws.r
-                  )
+                with rws as (
+                  select rownum r from dual connect by level <= 2
+                )
+                SELECT Undercast_length,
+                  case r
+                    when 1 then TRUNC(delay_stop_shift, 'DDD')
+                    else delay_start_shift
+                    end as startS,
+                  case r
+                    when 2 then 
+                    CASE 
+                      WHEN EXTRACT(DAY from cast(delay_start_shift as timestamp)) <> EXTRACT(DAY from cast(delay_stop_shift as timestamp))  
+                      THEN TRUNC(delay_stop_shift, 'DDD')
+                    else delay_stop_shift
+                    END
+                  else delay_stop_shift  
+                  end as stopS
+                FROM (
+                  SELECT (length_at_end - length_at_beginning) * (  -0.01 *percentage_of_deviation / (1-0.01 * percentage_of_deviation)) as Undercast_length,
+                    start_downtime, 
+                    end_downtime,
+                    start_downtime + INTERVAL '4' hour + INTERVAL '30' minute as delay_start_shift,
+                    end_downtime + INTERVAL '4' hour + INTERVAL '30' minute as delay_stop_shift
+                  FROM L2PRD.REP_CCM_DOWNTIME, p
+                  WHERE  (start_downtime + INTERVAL '4' hour + INTERVAL '30' minute >= p.startT
+                AND start_downtime + INTERVAL '4' hour + INTERVAL '30' minute < p.stopT)
+                    OR
+                    (end_downtime + INTERVAL '4' hour + INTERVAL '30' minute >= p.startT
+                AND end_downtime + INTERVAL '4' hour + INTERVAL '30' minute < p.stopT)
+                )
+                JOIN rws on case 
+                      when EXTRACT(DAY from cast(delay_start_shift as timestamp)) = EXTRACT(DAY from cast(delay_stop_shift as timestamp)) 
+                      then 2 
+                      else 1 
+                      end <= rws.r
               )
-              GROUP BY delayDate 
+            )
+            GROUP BY delayDate 
           ) DELAYS 
           ON DELAYS.delayDate = STOP_DATE 
           ORDER BY STOP_DATE 
